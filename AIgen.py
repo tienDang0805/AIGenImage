ec7928824599a4ab2d240ccdef572bb926f91f53
tienDang0805 <81803058+tienDang0805@users.noreply.github.com>
2024-12-18 10:41:11 +0700

    x

--- AIgen.py
+++ AIgen.py
@@ -3,23 +3,21 @@ import google.generativeai as genai
 
 genai.configure(api_key="AIzaSyCEphH-oXMWMGkNkWmiTfjvWS1QRjtSb1o")
 
-models = genai.list_models()
-for model in models.models:
-    print(model.name)
-# imagen = genai.ImageGenerationModel("imagen-3.0-generate-001")
 
-# result = imagen.generate_images(
-#     prompt="Fuzzy bunnies in my kitchen",
-#     number_of_images=4,
-#     safety_filter_level="block_only_high",
-#     person_generation="allow_adult",
-#     aspect_ratio="3:4",
-#     negative_prompt="Outside",
-# )
+imagen = genai.ImageGenerationModel("imagen-3.0-generate-001")
 
-# for image in result.images:
-#   print(image)
+result = imagen.generate_images(
+    prompt="Fuzzy bunnies in my kitchen",
+    number_of_images=4,
+    safety_filter_level="block_only_high",
+    person_generation="allow_adult",
+    aspect_ratio="3:4",
+    negative_prompt="Outside",
+)
 
-# # Open and display the image using your local operating system.
-# for image in result.images:
-#   image._pil_image.show()
+for image in result.images:
+  print(image)
+
+# Open and display the image using your local operating system.
+for image in result.images:
+  image._pil_image.show()
